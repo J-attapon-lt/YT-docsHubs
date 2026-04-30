ff012656600fc7f30835bcc825dfcb2a641fb176
Attapon.J <90230975+J-attapon-lt@users.noreply.github.com>
2026-04-30 15:22:35 +0000

    Resolve main jsx merge conflict

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-<<<<<<< HEAD
-=======
-import 'bootstrap-icons/font/bootstrap-icons.css';
->>>>>>> 05764f027d5377381596e275c5567f51b945830b
 import './index.css';
 import App from './App.jsx';
 
