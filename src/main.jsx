77d9161ea666f7e45b9c777c27bc2486d719e9a2
Attapon.J <90230975+J-attapon-lt@users.noreply.github.com>
2026-04-30 13:45:57 +0000

    css

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import './index.css';
 import App from './App.jsx';
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
